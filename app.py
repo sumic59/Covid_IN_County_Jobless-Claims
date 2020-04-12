7567c254469d26b2161aa4f837d5fbb7a9a13fe8
Michael Davis <57714753+Davis1776@users.noreply.github.com>
2020-04-11 22:33:48 -0400

    MRD: add route for all 12 counties

--- app.py
+++ app.py
@@ -21,17 +21,53 @@ def welcome():
     message = "The Reaper Project"
     return render_template("index.html", message=message)
 
+@app.route("/Allen.html")
+def Allen():
+    return render_template("Allen.html")
+
+@app.route("/Boone.html")
+def Boone():
+    return render_template("Boone.html")
+
+@app.route("/Decatur.html")
+def Decatur():
+    return render_template("Decatur.html")
+
+@app.route("/Hamilton.html")
+def Hamilton():
+    return render_template("Hamilton.html")
+
+@app.route("/Hendricks.html")
+def Hendricks():
+    return render_template("Hendricks.html")
+
+@app.route("/Johnson.html")
+def Johnson():
+    return render_template("Johnson.html")
+
+@app.route("/Lake.html")
+def Lake():
+    return render_template("Lake.html")
+
 @app.route("/Marion.html")
 def vanderburgh():
     return render_template("Marion.html")
 
 @app.route("/Monroe.html")
-def vanderburgh():
+def Monroe():
     return render_template("Monroe.html")
 
 @app.route("/StJoseph.html")
-def vanderburgh():
+def StJoseph():
     return render_template("StJoseph.html")
+
+@app.route("/Vanderburgh.html")
+def Vanderburgh():
+    return render_template("Vanderburgh.html")
+
+@app.route("/Warrick.html")
+def Warrick():
+    return render_template("Warrick.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
